e70eb78cb420c8d95b9aa82f1ac7da18045093f1
T-unity <takadanaritoshi@gmail.com>
2023-01-22 03:06:47 +0900

    Rewrite

--- App.tsx
+++ App.tsx
@@ -5,35 +5,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Components
+import Main from './src/views/tabComponents/First';
 import DetailsScreen from './src/views/DetailsScreen';
 import TabBar from './src/views/TabBar';
-
-// Define footer tabs
-function Main({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={styles.container}>
-
-        <StatusBar style="auto" />
-
-        <View style={[styles.base]}>
-          <Text style={styles.text}>Hoge</Text>
-          <Button
-            title="詳細を見る"
-            onPress={() => {
-              // navigate with params
-              navigation.navigate('詳細画面', {
-                indexId: 1,
-                subText: 'hogehogehoge',
-              });
-            }}
-          />
-        </View>
-
-      </View>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 function App() {
